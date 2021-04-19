64e38466cf5e839b4de9b8190c2cbdf5759140be
Cherissa Woudenberg <snerzel@comcast.net>
2021-04-19 18:24:48 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,4 +1,13 @@
 # Your code goes here!
+attr_accessor :letters
 class Anagram
+  def initialize(letters)
+    @letters = letters
+  end
 
+  def match(string)
+    string.each do |word|
+      word.split.sort == letters.split.sort
+    end
+  end
 end
